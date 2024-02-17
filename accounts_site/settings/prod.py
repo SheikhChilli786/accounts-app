8943d1b8aa640397bacc8aaa9bd9842863a26e8a
Anas <2122yahooo@gmail.com>
2024-02-17 19:55:12 +0500

    ...

--- accounts_site/settings/prod.py
+++ accounts_site/settings/prod.py
@@ -2,7 +2,7 @@ import dj_database_url
 from .common import *
 import os
 DEBUG = False
-ALLOWED_HOSTS = ['hisaabkitaab.online']
+ALLOWED_HOSTS = ['*']
 
 SECRET_KEY = os.environ['SECRET_KEY']
 DATABASES = {
