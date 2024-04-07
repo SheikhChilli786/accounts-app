c47ac71b0096fb502f4196b3c2525c1c9acac1ee
Anas <2122yahooo@gmail.com>
2024-04-07 20:48:37 +0500

    core:user models staff user

--- core/models.py
+++ core/models.py
@@ -6,3 +6,14 @@ class User(AbstractUser):
     phone_number = models.CharField(unique=True)
     question = models.TextField(blank=True,null=True)
     answer = models.TextField(blank=True,null=True)
+    assigned_staff = models.ForeignKey('StaffUser',on_delete=models.SET_NULL,blank=True,null=True,related_name="assigned_staff")
+    class Meta:
+        permissions = (
+            ("can_assign_staff","Can Assign Staff to Users"),
+        )
+
+
+class StaffUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.SET_NULL,null=True)
+    def __str__(self) -> str:
+        return f"{self.user}"
