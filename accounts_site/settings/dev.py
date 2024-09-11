7e978f616c4475165ec2161f31829e572666e2ed
Niersol <anasusman0324@gmail.com>
2024-09-11 15:49:51 +0500

    changed dev database

--- accounts_site/settings/dev.py
+++ accounts_site/settings/dev.py
@@ -5,7 +5,7 @@ SECRET_KEY = 'django-insecure-*(ex1m!q^a5!)hk&l=ixu)#&j=-*p1-yn(j^2r+_79%ne4)m(&
 DATABASES = {
     'default': {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "accounts",
+        "NAME": "accounts_app",
         "USER": "postgres",
         "PASSWORD": "45645688mm",
         "HOST": "127.0.0.1",
